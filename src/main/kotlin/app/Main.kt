9493fa651c5e81888461813eb8b62cbbaa8be4dc
Sascha Fischer <moh.sushi@googlemail.com>
2021-11-09 10:12:41 +0100

    added "/" for ui-contextPath

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -20,7 +20,7 @@ fun main() {
     }.start(80)
 
     Javalin.create {
-        it.contextPath = "ui"
+        it.contextPath = "/ui"
         it.addStaticFiles("/public", Location.CLASSPATH)
     }.start(8081)
 
